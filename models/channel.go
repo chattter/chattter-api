a02026fe727995c360fa6b7442312adf17b6e267
parkerdouglass <parker.douglass@icloud.com>
2021-09-21 16:57:47 -0700

    Removed Logo column in channel.go

--- models/channel.go
+++ models/channel.go
@@ -2,14 +2,12 @@ package models
 
 import (
 	"database/sql"
-	"image"
 	"time"
 )
 
 type Channel struct {
 	ID          uint64 `gorm:"primaryKey"`
 	Name        string
-	Logo        image.Image
 	CreatedDate time.Time
 	DeletedDate sql.NullTime
 }
