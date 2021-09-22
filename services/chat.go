58c9e631196e2821968780af5a2b0522b0f668f8
parkerdouglass <parker.douglass@icloud.com>
2021-09-21 17:33:09 -0700

    Added functions to the chat service

--- services/chat.go
+++ services/chat.go
@@ -1,8 +1,10 @@
 package services
 
 import (
-	"errors"
+	"database/sql"
+	"time"
 
+	"github.com/chattter/chattter-api/models"
 	"gorm.io/gorm"
 )
 
@@ -10,6 +12,41 @@ type ChatService struct {
 	DB *gorm.DB
 }
 
-func (s *ChatService) SendMessage() error {
-	return errors.New("SendMessage not implemented")
+func (s *ChatService) SendMessage(text string, channelID uint64, senderID uint64) (*models.Message, error) {
+	message := models.Message{
+		AccountID:   senderID,
+		ChannelID:   channelID,
+		Message:     text,
+		CreatedDate: time.Now(),
+	}
+	if err := s.DB.Create(&message).Error; err != nil {
+		return nil, err
+	}
+	return &message, nil
+
+}
+
+func (s *ChatService) DeleteMessage(id uint64, accountID uint64) error {
+	return s.DB.
+		Where("id = ?", id).
+		Where("account_id = ?", accountID).
+		Update(
+			"deleted_date",
+			sql.NullTime{
+				Time:  time.Now(),
+				Valid: true,
+			},
+		).
+		Error
+}
+
+func (s *ChatService) EditMessage(id uint64, message string) error {
+	return s.DB.
+		Where("id = ?", id).
+		Where("message = ?", message).
+		Update(
+			"message",
+			message,
+		).
+		Error
 }
